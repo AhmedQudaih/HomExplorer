b8def016217ad20df4da2b7f1704b9e4dd49b58f
MomenMahmoud266 <94162374+MomenMahmoud266@users.noreply.github.com>
2022-01-06 19:04:07 +0200

    3rd

--- client/src/components/FavouriteEstate.js
+++ client/src/components/FavouriteEstate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button , Box  } from '@mui/material';
 import {
     EstateMainForm,
     EstateFormTitle,
@@ -10,11 +11,44 @@ import {
   import EstateCard from './estateCard';
   import {Button, Dialog ,DialogContent } from '@mui/material';
   import { Close as CloseIcon} from "@material-ui/icons";
+  import serverFunctions from '../serverFunctions/estate';
 
-function saveEstate (props){
-const [estate, setEstate] = useState(props.data);
+function favouriteEstate (props){
+const [estate, setEstate] = React.useState(props.data);
   const handleClose = () => setOpen(false);
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [expand, setExpand] = React.useState(false);
+  const [data, setData] = React.useState('');
+  const [page, setPage] = React.useState(1);
+  const [change, setChange] = React.useState(false);
+  const [partition, setPartition] = React.useState(0);
+  const totalPages = Math.ceil(data.length / 6);
+  const handlePageChange = (event, value) => {
+    setPage(value);
+    if(totalPages - value <= 1 ){
+      getPartion();
+    }
+    const getPartion = () => {
+      setPartition((pre)=>{
+        return (pre + 1);
+      })
+    };
+  const handleDetailsClick = (id) => {
+    setExpand(id);
+  };
+  React.useEffect(() => {
+    const fetchData = async ()=>{
+      const data = await serverFunctions.getEstates(partition);
+      setData((pre)=>{
+        return[
+          ...pre,
+          ...data
+        ]
+      });
+    }
+
+    fetchData();
+  },[partition, change])
 
 return(
      <div>
@@ -26,7 +60,14 @@ return(
     </div>
 <DialogContent>
     <EstateMainForm>
-    <EstateCard updateData={updateData} key={e._id} data={e} expand={expand} handleDetailsClick={handleDetailsClick} />
+    {
+          data.slice((Math.ceil(page) - 1) * 6, Math.ceil(page) * 6).map((e) => (
+            <EstateCard  key={e._id} data={e} expand={expand} handleDetailsClick={handleDetailsClick} />
+           ))
+        }
+    <Box sx={{ m: "2%", padding: "0.5%",backgroundColor: 'white',borderRadius: "1rem"}}>
+      <Button href="#services"><Pagination count={totalPages} page={page} color="success" onChange={handlePageChange}/></Button>
+    </Box>
     </EstateMainForm>
 </DialogContent>
 </Dialog>
@@ -36,4 +77,4 @@ return(
 );
 
 };
-export default saveEstate;
+export default favouriteEstate;

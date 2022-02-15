5e7c8a4f13dc80995b7e2860917f53f599fd31f6
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-02-15 16:49:09 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import {BrowserRouter  as Router, Route, Routes} from 'react-router-dom';
-/*import Sidebar from './components/sidebar.js';
+import Sidebar from './components/sidebar.js';
 import Navbar from './components/navbar.js';
 import Footer from './components/footer.js';
 import Home from './pages/homePage.js';
@@ -9,41 +9,24 @@ import Products from './pages/productsPage.js';
 import Admin from './pages/admin.js';
 import Provider from './components/provider';
 import NotFound404Component from './components/notFound404Component';
-*/function App() {
+function App() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = (status) => {
     setIsOpen(status);
   }
-  return (
-
-      <Router basename="/">
-        <Routes >
-          <Route exact path="" element={<h1>home</h1>}/>
-          <Route exact path="products" element={<h1>Products</h1>}/>
-        </Routes >
-
-      </Router>
-
-  );
+  return (<Provider>
+    <Router basename="/">
+      <Sidebar isOpen={isOpen} toggle={toggle}/>
+      <Navbar toggle={toggle}/>
+      <Routes >
+        <Route exact path="" element={<Home />}/>
+        <Route exact path="products" element={<Products />}/>
+        <Route exact path="admin" element={<Admin />}/>
+        <Route exact path='*' element={<NotFound404Component />} />
+      </Routes >
+      <Footer/>
+    </Router>
+  </Provider>);
 }
 
 export default App;
-
-
-/*
-<Provider>
-  <Router basename="/">
-    <Sidebar isOpen={isOpen} toggle={toggle}/>
-    <Navbar toggle={toggle}/>
-    <Routes >
-      <Route exact path="" element={<Home />}/>
-      <Route exact path="products" element={<Products />}/>
-      <Route exact path="admin" element={<Admin />}/>
-      <Route exact path='*' element={<NotFound404Component />} />
-    </Routes >
-    <Footer/>
-  </Router>
-</Provider>
-
-
-*/

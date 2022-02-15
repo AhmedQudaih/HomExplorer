f0893e4db1be036218bb11374cacda6a65261ff8
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-02-15 16:29:07 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
-/*import {BrowserRouter  as Router, Route, Routes} from 'react-router-dom';
-import Sidebar from './components/sidebar.js';
+import {BrowserRouter  as Router, Route, Routes} from 'react-router-dom';
+/*import Sidebar from './components/sidebar.js';
 import Navbar from './components/navbar.js';
 import Footer from './components/footer.js';
 import Home from './pages/homePage.js';

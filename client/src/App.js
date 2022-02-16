c84fca6529f15403fe86514d1d0fa8cd2bf7aaf2
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-02-16 18:04:10 +0200

    Revert "add jsconfig and update app"
    This reverts commit ea51e7887292ebc9a7055949ba2aade602de0b09.

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,28 @@
 import React, {useState} from 'react';
 import './App.css';
 import {BrowserRouter  as Router, Route, Routes} from 'react-router-dom';
-import Sidebar from './components/sidebar.js';
+//import Sidebar from './components/sidebar.js';
 import Navbar from './components/navbar.js';
-import Footer from './components/footer.js';
+/*import Footer from './components/footer.js';
 import Home from './pages/homePage.js';
 import Products from './pages/productsPage.js';
 import Admin from './pages/admin.js';
 import Provider from './components/provider';
 import NotFound404Component from './components/notFound404Component';
-function App() {
+*/function App() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = (status) => {
     setIsOpen(status);
   }
-  return (<Provider>
+  return (
     <Router basename="/">
-      <Sidebar isOpen={isOpen} toggle={toggle}/>
       <Navbar toggle={toggle}/>
       <Routes >
-        <Route exact path="" element={<Home />}/>
-        <Route exact path="products" element={<Products />}/>
-        <Route exact path="admin" element={<Admin />}/>
-        <Route exact path='*' element={<NotFound404Component />} />
-      </Routes >
-      <Footer/>
+        <Route exact path="" element={<h1>hereee</h1>}/>
+        </Routes >
+
     </Router>
-  </Provider>);
+);
 }
 
 export default App;

7099cd9d24228b5f46035afadeff36e9adb85365
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-02-16 17:03:59 +0200

    Revert "Update App.js"
    This reverts commit 8544037effea9554db348fe699bae9bd47d80746.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
 import './App.css';
 import {BrowserRouter  as Router, Route, Routes} from 'react-router-dom';
-import Sidebar from 'components/sidebar.js';
-import Navbar from 'components/navbar.js';
-import Footer from 'components/footer.js';
-import Home from 'pages/homePage.js';
-import Products from 'pages/productsPage.js';
-import Admin from 'pages/admin.js';
-import Provider from 'components/provider';
-import NotFound404Component from 'components/notFound404Component';
+import Sidebar from './components/sidebar.js';
+import Navbar from './components/navbar.js';
+import Footer from './components/footer.js';
+import Home from './pages/homePage.js';
+import Products from './pages/productsPage.js';
+import Admin from './pages/admin.js';
+import Provider from './components/provider';
+import NotFound404Component from './components/notFound404Component';
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = (status) => {

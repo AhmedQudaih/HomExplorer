71c62eb5dc58dc64077c3b378478c2fee9f6123e
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-02-16 17:37:37 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
 import {BrowserRouter  as Router, Route, Routes} from 'react-router-dom';
-import Sidebar from './components/sidebar.js';
+//import Sidebar from './components/sidebar.js';
 import Navbar from './components/navbar.js';
-import Footer from './components/footer.js';
+/*import Footer from './components/footer.js';
 import Home from './pages/homePage.js';
 import Products from './pages/productsPage.js';
 import Admin from './pages/admin.js';
 import Provider from './components/provider';
 import NotFound404Component from './components/notFound404Component';
-function App() {
+*/function App() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = (status) => {
     setIsOpen(status);
@@ -19,11 +19,8 @@ function App() {
       <Sidebar isOpen={isOpen} toggle={toggle}/>
       <Navbar toggle={toggle}/>
       <Routes >
-        <Route exact path="" element={<Home />}/>
-        <Route exact path="products" element={<Products />}/>
-        <Route exact path="admin" element={<Admin />}/>
-        <Route exact path='*' element={<NotFound404Component />} />
-      </Routes >
+        <Route exact path="" element={<h1>hereee</h1>}/>
+        </Routes >
       <Footer/>
     </Router>
   </Provider>);
